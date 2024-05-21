cf53d0984961d9f7e559c97d9f36b6af254bc810
Miguel AJM <koekun3000@gmail.com>
2024-05-21 01:47:55 -0500

    Add validation and permission checks to create and update best stats

--- render/services/panel/stats/create.ts
+++ render/services/panel/stats/create.ts
@@ -2,16 +2,40 @@
 
 import { z } from 'zod'
 import { CharacterBestStatsSchema } from '@/schemas'
+import { currentRole } from '@/data/auth'
 import db from '@/libs/db'
 
 export const createBestStats = async (
   data: z.infer<typeof CharacterBestStatsSchema>
 ) => {
-  try {
-    const bestStats = await db.characterBestStat.create({
-      data
-    })
+  const currentAdminRole = await currentRole()
+  const validateFields = CharacterBestStatsSchema.safeParse(data)
 
+  if (currentAdminRole !== 'ADMIN' && currentAdminRole !== 'OWNER')
+    return {
+      error: 'No tienes permisos para realizar esta acción.',
+      status: 403
+    }
+
+  if (!validateFields.success)
+    return {
+      error: 'Campos inválidos.',
+      status: 400
+    }
+
+  const { circletStat, globetStat, sandStat, substatPriority } =
+    validateFields.data
+
+  const bestStats = await db.characterBestStat.create({
+    data: {
+      circletStat,
+      globetStat,
+      sandStat,
+      substatPriority
+    }
+  })
+
+  try {
     return { data: bestStats, message: 'Estadísticas agregadas!', status: 201 }
   } catch (error) {
     return {
--- render/services/panel/stats/update.ts
+++ render/services/panel/stats/update.ts
@@ -2,12 +2,40 @@
 
 import { z } from 'zod'
 import { CharacterBestStatsSchema } from '@/schemas'
+import { currentRole } from '@/data/auth'
 import db from '@/libs/db'
 
 export const updateBestStats = async (
   data: z.infer<typeof CharacterBestStatsSchema>,
   statId: string
 ) => {
+  const currentAdminRole = await currentRole()
+  const validateFields = CharacterBestStatsSchema.safeParse(data)
+
+  if (currentAdminRole !== 'ADMIN' && currentAdminRole !== 'OWNER')
+    return {
+      error: 'No tienes permisos para realizar esta acción.',
+      status: 403
+    }
+
+  if (!validateFields.success)
+    return {
+      error: 'Campos inválidos.',
+      status: 400
+    }
+
+  const { circletStat, globetStat, sandStat, substatPriority } =
+    validateFields.data
+
+  const bestStats = await db.characterBestStat.create({
+    data: {
+      circletStat,
+      globetStat,
+      sandStat,
+      substatPriority
+    }
+  })
+
   try {
     const bestStats = await db.characterBestStat.update({
       where: {
