bfa257c606df97fffcbb4ff81d96afe5a174ab48
Miguel AJM <koekun3000@gmail.com>
2024-07-06 10:25:32 -0500

    feat: Add wrapper component

--- render/sections/builds/section-build.tsx
+++ render/sections/builds/section-build.tsx
@@ -1,29 +1,17 @@
-import { Card, CardBody, CardHeader } from '@nextui-org/card'
 import { CharacterTypes } from '@/types'
-import CharacterFilter from '@/render/components/UI/filters/character-filter'
-import SearchCharacter from '@/render/components/home/characters/search-character'
 import ListBuilds from '@/render/components/home/builds/list-builds'
+import CharacterListWrapper from '@/render/components/UI/wrappers/character-list-wrapper'
 
-const SectionBuild = ({ characters }: { characters: CharacterTypes[] }) => {
+interface ListCharacterProps {
+  characters: CharacterTypes[]
+}
+
+const SectionBuild = ({ characters }: ListCharacterProps) => {
   return (
     <section className='rounded-xl'>
-      <Card className='bg-color-dark p-4'>
-        <CardHeader>
-          <div className='w-full space-y-5'>
-            <div className='w-full flex flex-wrap gap-4 justify-between items-center'>
-              <h2 className='text-base md:text-2xl font-bold'>
-                Lista de personajes
-              </h2>
-
-              <SearchCharacter />
-            </div>
-            <CharacterFilter />
-          </div>
-        </CardHeader>
-        <CardBody>
-          <ListBuilds characters={characters} />
-        </CardBody>
-      </Card>
+      <CharacterListWrapper title='Lista de builds' isFilter isSearchBar>
+        <ListBuilds characters={characters} />
+      </CharacterListWrapper>
     </section>
   )
 }
--- render/sections/home/section-home.tsx
+++ render/sections/home/section-home.tsx
@@ -1,8 +1,6 @@
-import { Card, CardBody, CardHeader } from '@nextui-org/card'
 import { CharacterTypes } from '@/types'
 import { ListCharacter } from '@/render/components/home/characters/list-character'
-import CharacterFilter from '@/render/components/UI/filters/character-filter'
-import SearchCharacter from '@/render/components/home/characters/search-character'
+import CharacterListWrapper from '@/render/components/UI/wrappers/character-list-wrapper'
 
 interface ListCharacterProps {
   characters: CharacterTypes[]
@@ -11,23 +9,9 @@ interface ListCharacterProps {
 const SectionHome = ({ characters }: ListCharacterProps) => {
   return (
     <section className='rounded-xl'>
-      <Card className='bg-color-dark p-4'>
-        <CardHeader>
-          <div className='w-full space-y-5'>
-            <div className='w-full flex flex-wrap gap-4 justify-between items-center'>
-              <h2 className='text-base md:text-2xl font-bold'>
-                Lista de personajes
-              </h2>
-
-              <SearchCharacter />
-            </div>
-            <CharacterFilter />
-          </div>
-        </CardHeader>
-        <CardBody>
-          <ListCharacter characters={characters} />
-        </CardBody>
-      </Card>
+      <CharacterListWrapper title='Lista de personajes' isFilter isSearchBar>
+        <ListCharacter characters={characters} />
+      </CharacterListWrapper>
     </section>
   )
 }
