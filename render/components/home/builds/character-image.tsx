1e77f70a94c6bb3513a3ba42678fba660ac3ef3f
Miguel AJM <koekun3000@gmail.com>
2024-07-06 07:07:28 -0500

    feat: Add the link to build cards

--- render/components/home/builds/character-image.tsx
+++ render/components/home/builds/character-image.tsx
@@ -4,6 +4,7 @@ import { getRole } from '@/utils/helpers/get-role'
 import { Chip, Image } from '@nextui-org/react'
 import Figure from '@/render/components/UI/misc/figure'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 const CharacterImage = ({
   character
@@ -12,21 +13,27 @@ const CharacterImage = ({
 }) => {
   const starCharacter = getStarBorderColor(character?.stars || 0)
 
+  const characterName = character?.name.toLowerCase().replace(/\s/g, '-')
+  const url = `/characters/character?name=${characterName}`
+
   return (
     <div className='flex items-center gap-5'>
-      <Figure
-        padding='p-0'
-        width='w-24 lg:w-32'
-        height='h-24 lg:h-32'
-        radius='rounded-lg'
-        className={clsx(' border-[1px]', starCharacter)}
-      >
-        <Image
-          className='w-full h-full object-cover'
-          src={character?.imageUrl!}
-          alt={`Personaje: ${character?.name}`}
-        />
-      </Figure>
+      <Link href={url}>
+        <Figure
+          padding='p-0'
+          width='w-24 lg:w-32'
+          height='h-24 lg:h-32'
+          radius='rounded-lg'
+          className={clsx(' border-[1px]', starCharacter)}
+        >
+          <Image
+            isZoomed
+            className='w-full h-full object-cover'
+            src={character?.imageUrl!}
+            alt={`Personaje: ${character?.name}`}
+          />
+        </Figure>
+      </Link>
       <article className='space-y-2'>
         <h2 className='capitalize text-lg font-medium'>{character?.name}</h2>
         <Chip className='capitalize rounded-md'>
