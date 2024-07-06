da74cd24e0b94115a77a33736d502cc232fbc656
Miguel AJM <koekun3000@gmail.com>
2024-07-06 13:40:46 -0500

    feat: Add team page

---
+++ app/teams/page.tsx
@@ -0,0 +1,16 @@
+import { CharacterTypes, SearchParamsTypes } from '@/types'
+import Header from '@/render/components/home/header/header'
+
+const TeamPage = ({ searchParams }: SearchParamsTypes) => {
+
+  return (
+    <>
+      <Header />
+      <main className='max-w-[1280px] my-10 mx-auto'>
+
+      </main>
+    </>
+  )
+}
+
+export default TeamPage
