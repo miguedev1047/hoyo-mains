2d6138c89131e2d5b2f3251684f856bebba46c98
Miguel AJM <koekun3000@gmail.com>
2024-07-11 18:58:32 -0500

    feat: Add getWeapon helper function to info-header.tsx

--- render/src/panel/character/info-header.tsx
+++ render/src/panel/character/info-header.tsx
@@ -6,10 +6,10 @@ import {
   AlertTitle
 } from '@/render/src/panel/character/shared/components/alert'
 import { CharacterType } from '@/render/src/types'
-import { getRole } from '@/render/src/panel/character/shared/utilities/helpers/get-role'
 import { getElementImage } from '@/render/src/panel/character/shared/utilities/helpers/get-element-image'
+import { getRole } from '@/render/src/panel/character/shared/utilities/helpers/get-role'
+import { getWeapon } from '@/render/src/panel/character/shared/utilities/helpers/get-weapon'
 import Figure from '@/render/src/shared/components/figure'
-import { getWeapon } from './shared/utilities/helpers/get-weapon'
 
 const InfoHeader = ({ character }: { character: CharacterType }) => {
   const weaponType = getWeapon(character?.weapon!)!
