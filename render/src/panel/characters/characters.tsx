a88f84a04542e2253d607b68ba603856b46e10cc
Miguel AJM <koekun3000@gmail.com>
2024-07-18 03:56:09 -0500

    feat: Remove unused code in Characters component

--- render/src/panel/characters/characters.tsx
+++ render/src/panel/characters/characters.tsx
@@ -1,14 +1,11 @@
 import { CharacterType } from '@/render/src/types'
 import CharacterList from '@/render/src/panel/characters/components/character-list'
-import CharacterNoItems from '@/render/src/panel/characters/components/no-items-found'
 
 interface CharactersTypes {
   characters: CharacterType[]
 }
 
 const Characters = ({ characters }: CharactersTypes) => {
-  if (!characters?.length) return <CharacterNoItems />
-
   return <CharacterList characters={characters} />
 }
 
