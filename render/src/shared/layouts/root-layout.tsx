6f3a52663f3c55842ef9da271668407196eb3fa3
Miguel AJM <koekun3000@gmail.com>
2024-07-19 21:59:07 -0500

    refactor: Update root layout with fixed background gradients

--- render/src/shared/layouts/root-layout.tsx
+++ render/src/shared/layouts/root-layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head />
       <body
         className={clsx(
-          'app-theme bg-background font-sans antialiased relative flex flex-col h-full p-2',
+          'app-theme bg-color-darkest font-sans antialiased relative flex flex-col h-full p-2',
           fontDM_Sans.variable
         )}
       >
@@ -29,7 +29,6 @@ export default function RootLayout({
           <Toaster position='top-right' richColors />
           <div className='fixed top-0 left-0 h-screen w-full bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
 
-          <div className='fixed bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]'></div>
           {children}
         </Providers>
       </body>
