198909333f3c3b9362e370bf1604e85a826c71ba
Miguel AJM <koekun3000@gmail.com>
2024-07-26 13:19:14 -0500

    feat: Add hero card to build list

--- render/src/builds/builds.tsx
+++ render/src/builds/builds.tsx
@@ -1,8 +1,7 @@
-import { Card, CardBody, CardHeader } from '@nextui-org/card'
-import { Search } from '@/render/src/shared/components/search'
 import { CharacterType } from '@/render/src/types'
-import BuildFilters from '@/render/src/builds/components/build-filters'
-import BuildList from '@/render/src/builds/components/build-list'
+import { Hero } from '@/render/src/shared/components/hero'
+import { HuTaoHeroBrand } from '@/assets/brands'
+import BuildContainer from '@/render/src/builds/containers/build-container'
 
 interface BuildsProps {
   builds: CharacterType[]
@@ -10,30 +9,16 @@ interface BuildsProps {
 
 const Builds = ({ builds }: BuildsProps) => {
   return (
-    <Card className='bg-color-dark p-4'>
-      <CardHeader>
-        <div className='w-full space-y-5'>
-          <div className='w-full flex flex-wrap gap-4 justify-between items-center'>
-            <h2 className='text-base md:text-2xl font-bold'>
-              Lista de personajes
-            </h2>
+    <div className='space-y-4'>
+      <Hero
+        src={HuTaoHeroBrand.src}
+        mainTitle='Builds'
+        title='Lista general de builds'
+        description='Aprende a construir a tus personajes favoritos con nuestra lista de builds. Encuentra la mejor combinación de artefactos, armas y talentos para maximizar el potencial de tu personaje.'
+      />
 
-            <Search
-              label='Buscar personaje'
-              placeholder='Hu tao...'
-              searchQuery='name'
-              className='max-w-[375px]'
-              variant='underlined'
-            />
-          </div>
-
-          <BuildFilters />
-        </div>
-      </CardHeader>
-      <CardBody>
-        <BuildList builds={builds} />
-      </CardBody>
-    </Card>
+      <BuildContainer builds={builds} />
+    </div>
   )
 }
 
---
+++ render/src/builds/containers/build-container.tsx
@@ -0,0 +1,38 @@
+import { Card, CardBody, CardHeader } from '@nextui-org/card'
+import { Search } from '@/render/src/shared/components/search'
+import { CharacterType } from '@/render/src/types'
+import BuildFilters from '@/render/src/builds/components/build-filters'
+import BuildList from '@/render/src/builds/components/build-list'
+
+interface BuildsProps {
+  builds: CharacterType[]
+}
+
+const BuildContainer = ({ builds }: BuildsProps) => {
+  return (
+    <Card className='bg-color-dark p-4'>
+      <CardHeader>
+        <div className='w-full space-y-5'>
+          <div className='w-full flex flex-wrap gap-4 justify-between items-center'>
+            <h2 className='text-base md:text-2xl font-bold'>Lista de builds</h2>
+
+            <Search
+              label='Buscar personaje'
+              placeholder='Hu tao...'
+              searchQuery='name'
+              className='max-w-[375px]'
+              variant='underlined'
+            />
+          </div>
+
+          <BuildFilters />
+        </div>
+      </CardHeader>
+      <CardBody>
+        <BuildList builds={builds} />
+      </CardBody>
+    </Card>
+  )
+}
+
+export default BuildContainer
