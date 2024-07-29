99fd026b1e5c0fd4c449867633a8dfc6bf6ead1f
Miguel AJM <koekun3000@gmail.com>
2024-07-29 13:40:22 -0500

    feat: Refactor character route to handle character name parameter

--- app/api/characters/character/route.ts
+++ app/api/characters/character/route.ts
@@ -1,65 +1,70 @@
 import { NextResponse } from 'next/server'
 import db from '@/libs/db'
+import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
   try {
     const nameParams = new URL(request.url).searchParams.get('name')!
-    const characterName = nameParams.replace(/-/g, ' ')
+    const characterName = nameParams?.replace(/-/g, ' ')
 
-    const characters = await db.character.findFirst({
-      where: { name: characterName },
-      include: {
-        materials: {
-          orderBy: {
-            order: 'asc'
-          }
-        },
-        weapons: {
-          orderBy: {
-            order: 'asc'
-          }
-        },
-        artifacts: {
-          orderBy: {
-            order: 'asc'
-          }
-        },
-        bestStats: true,
-        videoGuide: true,
-        teams: {
-          include: {
-            characters: true
-          }
-        },
-        talents: {
-          orderBy: {
-            createdDate: 'asc'
-          }
-        },
-        passives: {
-          orderBy: {
-            createdDate: 'asc'
-          }
-        },
-        constellations: {
-          orderBy: {
-            createdDate: 'asc'
-          }
-        },
-        ascensions: {
-          orderBy: [{ level: 'asc' }, { cost: 'asc' }],
-          include: {
-            materials: {
-              orderBy: {
-                order: 'asc'
+    if (!characterName) {
+      const characters = await db.character.findFirst({
+        where: { name: characterName },
+        include: {
+          materials: {
+            orderBy: {
+              order: 'asc'
+            }
+          },
+          weapons: {
+            orderBy: {
+              order: 'asc'
+            }
+          },
+          artifacts: {
+            orderBy: {
+              order: 'asc'
+            }
+          },
+          bestStats: true,
+          videoGuide: true,
+          teams: {
+            include: {
+              characters: true
+            }
+          },
+          talents: {
+            orderBy: {
+              createdDate: 'asc'
+            }
+          },
+          passives: {
+            orderBy: {
+              createdDate: 'asc'
+            }
+          },
+          constellations: {
+            orderBy: {
+              createdDate: 'asc'
+            }
+          },
+          ascensions: {
+            orderBy: [{ level: 'asc' }, { cost: 'asc' }],
+            include: {
+              materials: {
+                orderBy: {
+                  order: 'asc'
+                }
               }
             }
           }
         }
-      }
-    })
+      })
 
-    return NextResponse.json(characters, { status: 200 })
+      return NextResponse.json(characters, { status: 200 })
+    }
+
+    return redirect('/')
   } catch (error: any) {
     return NextResponse.json(
       {
