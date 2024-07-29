80ea2c304b9c6dbf6ac0b3f02a4606d0dd9e94ce
Miguel AJM <koekun3000@gmail.com>
2024-07-29 03:07:02 -0500

    feat: Update hero component

--- render/src/shared/components/hero/index.tsx
+++ render/src/shared/components/hero/index.tsx
@@ -1,5 +1,4 @@
 import { Figure } from '@/render/src/shared/components/figure'
-import { Image } from '@nextui-org/react'
 import { BoxCard, BoxCardBody } from '@/render/src/shared/components/box'
 
 interface HeroProps {
@@ -13,13 +12,23 @@ export const Hero = ({ description, src, title }: HeroProps) => {
     <BoxCard className='bg-color-dark'>
       <BoxCardBody>
         <div className='space-y-4 text-color-lightest max-md:rounded-md'>
-          <Figure className='bg-transparent h-36 relative' size='full'>
-            <div className='absolute inset-0 size-full bg-black/50 z-20 grid place-content-center'>
+          <Figure className='bg-transparent h-40 relative' size='full'>
+            <div
+              style={{
+                background: `url(${src})`,
+                backgroundAttachment: 'scroll',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
+              }}
+              className='w-full h-full'
+            />
+
+            <div className='absolute inset-0 flex items-center justify-center z-[10] size-full bg-black/50'>
               <h1 className='text-xl md:text-5xl font-bold uppercase'>
                 {title}
               </h1>
             </div>
-            <Image className='rounded-none' src={src} alt='Hu tao brand' />
           </Figure>
           <p className='text-xs sm:text-sm md:text-lg text-pretty'>
             {description}
